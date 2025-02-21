0a6da0df6238d96aed28fa7589b78a8b48c4205a
b1llybeane <80933177+XZ-Legendary-56@users.noreply.github.com>
2025-02-22 00:07:44 +0300

    Delete SoftwareTester-main directory

--- SoftwareTester-main/Assets/JS/main.js
+++
@@ -1,7 +0,0 @@
-/* dropdown */
-$(".dropdown").each((i, el) => {
-    $(el).find(".dropdownToggler").click(() => {
-        $(el).find(".dropdownToggler").find("img").toggleClass("rotate-180")
-        $(el).find(".dropdownList").toggle(500)
-    })
-})
